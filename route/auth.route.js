72369eb25ed4ab3cc8f45010342282659eda232c
Karthikselsons2023 <karthik@selsons.in>
2025-12-13 14:15:00 +0530

    changing auth/check

--- route/auth.route.js
+++ route/auth.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {login,logout } from "../controllers/auth.js";
+import {login,logout,checkAuth } from "../controllers/auth.js";
 import { protectRoute  } from "../middleware/auth.middleware.js";
-import {checkAuth} from "../controllers/auth.js";
+ 
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
