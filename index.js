6d24ed706b3c7c70c0ba50e4c08fa9dd63f0e877
Karthikselsons2023 <karthik@selsons.in>
2026-01-02 14:59:47 +0530

    link changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://192.168.1.18:5173","https://sunfood.world/"],
+  origin: ["http://localhost:5173", "http://192.168.1.18:5173","https://ripplecoin.in"],
   credentials: true
 }));
 
